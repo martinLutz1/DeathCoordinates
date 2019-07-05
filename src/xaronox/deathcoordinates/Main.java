28554d015f040c3acd730b550cc982512b67c841
martin <m.lutz@student.uni-tuebingen.de>
2019-07-05 11:48:13 +0200

    Convert tabs to spaces.

--- src/xaronox/deathcoordinates/Main.java
+++ src/xaronox/deathcoordinates/Main.java
@@ -11,55 +11,55 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	
-	private String getWorld(Player player) {
-		Environment playerEnvironment = player.getWorld().getEnvironment();
-		
-		if (playerEnvironment == Environment.NORMAL) {
-			return "World";
-		}
-		else if (playerEnvironment == Environment.NETHER) {
-			return "Nether";
-		}
-		else if (playerEnvironment == Environment.THE_END) {
-			return "End";
-		}
-		else {
-			return "Unknown environment";
-		}
-	}
-	 
+    
+    private String getWorld(Player player) {
+        Environment playerEnvironment = player.getWorld().getEnvironment();
+        
+        if (playerEnvironment == Environment.NORMAL) {
+            return "World";
+        }
+        else if (playerEnvironment == Environment.NETHER) {
+            return "Nether";
+        }
+        else if (playerEnvironment == Environment.THE_END) {
+            return "End";
+        }
+        else {
+            return "Unknown environment";
+        }
+    }
+     
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-    	Player deadPlayer = event.getEntity();
-    	
-    	Location deadPlayerLocation = deadPlayer.getLocation();
-    	int x = deadPlayerLocation.getBlockX();
-    	int y = deadPlayerLocation.getBlockY();
-    	int z = deadPlayerLocation.getBlockZ();
-    	
-    	// Death message for the player.
-    	String deathMessagePlayer = 
-    			"You died at " + 
-    			ChatColor.AQUA + x + " " + y + " " + z + " " +
-    			ChatColor.AQUA + "(" + getWorld(deadPlayer) + ")";
-    	deadPlayer.sendMessage(deathMessagePlayer);
-    	
-    	// Death message for the console (logging).
-    	String deathMessageConsole = 
-    			deadPlayer.getName() + " died at " + 
-    			x + " " + y + " " + z + " " +
+        Player deadPlayer = event.getEntity();
+        
+        Location deadPlayerLocation = deadPlayer.getLocation();
+        int x = deadPlayerLocation.getBlockX();
+        int y = deadPlayerLocation.getBlockY();
+        int z = deadPlayerLocation.getBlockZ();
+        
+        // Death message for the player.
+        String deathMessagePlayer = 
+                "You died at " + 
+                ChatColor.AQUA + x + " " + y + " " + z + " " +
+                ChatColor.AQUA + "(" + getWorld(deadPlayer) + ")";
+        deadPlayer.sendMessage(deathMessagePlayer);
+        
+        // Death message for the console (logging).
+        String deathMessageConsole = 
+                deadPlayer.getName() + " died at " + 
+                x + " " + y + " " + z + " " +
                 "(" + getWorld(deadPlayer) + ")";
-    	getLogger().info(deathMessageConsole);
+        getLogger().info(deathMessageConsole);
     }
     
     @Override
     public void onEnable() {
-    	Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
    
     @Override
     public void onDisable() {
-    	// Nothing to do :)
+        // Nothing to do :)
     }
 }
